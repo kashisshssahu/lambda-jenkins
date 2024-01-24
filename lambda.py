be295af8f86effddfddf0259a4349b5ffc2fef82
kashisshssahu <150845598+kashisshssahu@users.noreply.github.com>
2024-01-24 23:21:01 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -3,9 +3,9 @@ import boto3
 def lambda_handler(event, context):
     client = boto3.client('ec2', region_name= 'us-east-1')
     response = client.run_instances(
-           ImageId='ami-0e9107ed11be76fde',
+           ImageId='ami-0a3c3a20c09d6f377',
            InstanceType='t2.micro',
-           KeyName='docker',
+           KeyName='boto3',
            MaxCount=1,
            MinCount=1
            )
