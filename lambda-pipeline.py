1ce04c822f0ecf1147a1ec3fc024405900042738
kashisshssahu <150845598+kashisshssahu@users.noreply.github.com>
2024-01-24 23:18:42 +0530

    Update lambda-pipeline.py

--- lambda-pipeline.py
+++ lambda-pipeline.py
@@ -2,13 +2,13 @@ import boto3
 lambda_Client = boto3.client('lambda', region_name='us-east-1')
 response =lambda_Client.create_function(
           Code={
-                'S3Bucket': 'cloudeng-buck',
+                'S3Bucket': 'kashisshlambda',
                 'S3Key': 'lambda.zip', #how can i create or fetch this S3Key
           },
           Description='Process image objects from Amazon S3.',
           FunctionName="check",
           Handler='lambda.lambda_handler',
           Publish=True,
-          Role='arn:aws:iam::987075663466:role/lambda-boto3',
+          Role='arn:aws:iam::260723280636:role/jenkins_lambda',
           Runtime='python3.12'
           )
